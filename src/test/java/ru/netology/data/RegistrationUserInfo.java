7cfa92cfe2f77457edfe4f4bfe78785771a582af
Alex Vergizov <alvergizoff@gmail.com>
2023-06-03 21:58:27 +0300

    first test added

--- src/test/java/ru/netology/data/RegistrationUserInfo.java
+++
@@ -1,9 +0,0 @@
-package ru.netology.data;
-
-import lombok.Data;
-
-@Data
-public class RegistrationUserInfo {
-    private final String login;
-    private final String password;
-    private final String status;}
